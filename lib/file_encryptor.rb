ed42c69124d8e934502c2e87872c1ad27c3fc0b2
Philip Szalwinski <pszalwinski@gmail.com>
2015-03-28 14:45:02 -0500

    Write to encrypted file, WIP: encrypt the line

--- lib/file_encryptor.rb
+++ lib/file_encryptor.rb
@@ -1,6 +1,16 @@
 class FileEncryptor
-  def output(file)
+
+  def encrypt_line(file)
     path_to_encrypted_files = File.expand_path(".") + "/encrypted_files"
-    File.new("#{path_to_encrypted_files}"+"/encrypted_#{file.to_path}", "w+")
+    file_to_encrypt = File.open("#{path_to_encrypted_files}"+"/encrypted_#{file.to_path}", "w+")
+    output(file)
+  end
+
+  def output(file, text=nil)
+    path_to_encrypted_files = File.expand_path(".") + "/encrypted_files"
+    file = File.new("#{path_to_encrypted_files}"+"/encrypted_#{file.to_path}", "w+")
+
+    file.write(text)
+    file
   end
 end
--- spec/file_encryptor_spec.rb
+++ spec/file_encryptor_spec.rb
@@ -1,7 +1,7 @@
 require "file_encryptor"
 
 describe FileEncryptor do
-  xit "reads in a line froma text file and outputs encrypted line" do
+  it "reads in a line from a text file and outputs encrypted line" do
     file_encryptor = described_class.new
     test_file = File.new("test_file.txt", "w+") {|file| file.write("hello world")}
 
@@ -22,9 +22,24 @@ describe FileEncryptor do
     file_encryptor.output(test_file1)
 
     path_to_encrypted_files = File.expand_path(".") + "/encrypted_files"
-    File.exist?("#{path_to_encrypted_files}/encrypted_test_file1.txt").should be_true
+    File.exist?("#{path_to_encrypted_files}/encrypted_test_file1.txt").should be_truthy
 
     File.delete("test_file1.txt")
     File.delete("#{path_to_encrypted_files}/encrypted_test_file1.txt")
   end
+
+  it "adds a line of text to the file" do
+    file_encryptor = described_class.new
+    test_file1 = File.new("test_file1.txt", "w+")
+
+    path_to_encrypted_files = File.expand_path(".") + "/encrypted_files"
+    encrypted_file = File.open("#{path_to_encrypted_files}/encrypted_test_file.txt", "r")
+    encrypted_file = file_encryptor.output(test_file1, "hyieee")
+
+    encrypted_file.size
+    contents = File.open(encrypted_file, 'r') {|f| f.gets}
+
+    expect(contents).to eq("hyieee")
+  end
+
 end
